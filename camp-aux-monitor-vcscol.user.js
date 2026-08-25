0a861fd3ba27a4b1c7c108648b03456fb63ae455
yalnunez <yalnunez@amazon.com>
2026-08-25 12:48:34 -0500

    Update webhook URL for luribesa

--- camp-aux-monitor-vcscol.user.js
+++ camp-aux-monitor-vcscol.user.js
@@ -93,7 +93,7 @@ while (!BOT_OPERATOR) {
         'robayotl': 'https://hooks.chime.aws/incomingwebhooks/854ac974-16f9-4933-bff8-27eea4787851?token=Y0pGdnRkS2p8MXxPQXdGTEdoazVuSVg1VlVvVFB1RG9SbFRadXMwZE1WeGlUX0VBVmc4RVQ0',
         'cruizher': 'https://hooks.chime.aws/incomingwebhooks/c1d2932a-81e9-4cd6-a081-27b42c789556?token=aFpVUm9RUm58MXxXalBfTVZ6Sk9WaXF5TXk2ZGhlTjlzck1ENGFPQ0pWS2d5ZjgwWFhXOHlv',
         'llandine': 'https://hooks.chime.aws/incomingwebhooks/adc38fcf-a13f-4523-a5c1-f5610e78bd02?token=NlBqRDN1ODJ8MXxiN3F0Q2ZoMlVrV3lnaG81d1QxQldpZkVObXMxN1RmbVFMR0UtRGUwdkh3',
-        'luribesa': 'https://hooks.chime.aws/incomingwebhooks/206fb198-4995-43d3-9785-ca27ed5da95f?token=dm1Fa0JiSE18MXxnbDBzVXZKX3AyUDYxZTVkVTZJMjNvUXJENXJSNU85R0JseGFxS1BPTlZv',
+        'luribesa': 'https://hooks.chime.aws/incomingwebhooks/5366bd61-770d-4ace-a496-1870010d1317?token=b21MeWpMcjZ8MXx5cmxfbm5lbVFlRUJCdWdGV2FBbDhJVVRpZW1mSTFRTGZjb2huZjhNTXZn',
         'camargis': 'https://hooks.chime.aws/incomingwebhooks/d167f698-1c19-4780-9fe6-00251348bd3f?token=RGoyTlVHSFB8MXxxNGZIVi1vZWs0Ny0wYW42ckN1YzE4UXpWb0RNaDd6YXpqQ0diZm5jZHdB',
         'claraaqu': 'https://hooks.chime.aws/incomingwebhooks/6cc7fcfa-e146-47b0-a077-86d535cfb4eb?token=VlhCc1FwdUl8MXxVVkdDN1dYMjdwemY2RlM4M1RKZ2FFS0x2X1pkMjNUSm5FcWthMnVIWkpF',
         'jcaldani': 'https://hooks.chime.aws/incomingwebhooks/b764256b-33e2-466f-b795-db1e4c026c5a?token=VVE1U2prM018MXxZLVVwRUQ1TW1HZFNXSnQ3Y3FtR0JsOUZGUllKUzdoUEwzZGhvZHZxX2pJ',
