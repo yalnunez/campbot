90753f16a9176dfacd981cf4fa95f31b7ebb00a5
yalnunez <yalnunez@amazon.com>
2026-09-21 14:16:33 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- camp-aux-monitor-stores-vcscol.user.js
+++ camp-aux-monitor-stores-vcscol.user.js
@@ -570,7 +570,7 @@ initializeWebhooks().then(function(ready) {
         startMinute: 0,
         endHour: 23,// 23:00
         endMinute: 0,
-        graceMinutes: 5// 5 min gracia → activo de 00:55 a 23:05
+        graceMinutes: 10// 5 min gracia → activo de 00:50 a 23:10
     };
 
    function isOutsideOperationHours() {
